e5d3c83baed1d4d594cc3a9a2ce97264c9c12a79
unknown <toegelanna@outlook.com>
2019-08-14 16:47:15 +0200

    fixed

--- Week3/homework/MY-step2-7-Different_Results.js
+++ Week3/homework/MY-step2-7-Different_Results.js
@@ -22,6 +22,5 @@ console.log(y);
 
 // Add your explanation as a comment here
 
-//x is 9  and does not change after the f1 function is executed but it does if I console.log(f1(x)), I do not know why. Maybe because x was defined outside of the f1 function scope
-
-//y is returned after going through the f2 function where the value of y(that is an object containing x) is changed by adding 1 to x. 9+1=10
+// in function f1 x is passed by value, therefore does not change
+//in function f2 x is passed by reference, y is returned after going through the f2 function where the value of y(that is an object containing x) is changed by adding 1 to x. 9+1=10
